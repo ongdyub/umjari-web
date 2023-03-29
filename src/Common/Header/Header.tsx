5fe081b5b57a7770da6efc9ceec2feda2de808e1
ongdyub <ongdyub>
2023-03-29 16:04:58 +0900

    login btn design

--- src/Common/Header/Header.tsx
+++ src/Common/Header/Header.tsx
@@ -6,7 +6,7 @@ import {
     Container, Divider,
     IconButton,
     Menu,
-    MenuItem,
+    MenuItem, Stack,
     Toolbar,
     Tooltip,
     Typography, useMediaQuery, useTheme
@@ -19,12 +19,12 @@ import { useNavigate } from "react-router"
 
 var w = window.innerWidth
 const pages = ['홈', '커뮤니티', '단체검색하기', '중고거래 및 대여', '객원모집'];
-const settings = ['프로필', '마이페이지', '설정', '로그아웃', 'Size'];
+const settings = ['마이페이지', '작성기록', '설정', '로그아웃', 'Size'];
 
 const Header = () => {
 
     const theme = useTheme();
-    const res400 = useMediaQuery(theme.breakpoints.down("res400"))
+    const res450 = useMediaQuery(theme.breakpoints.down("res450"))
 
     const userState = useSelector(selectUser)
 
@@ -35,7 +35,6 @@ const Header = () => {
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElNav(event.currentTarget);
-        console.log("open nav")
     };
     const handleCloseNavMenu = (page : string) => {
         setAnchorElNav(null);
@@ -56,32 +55,34 @@ const Header = () => {
         else if(page === '객원모집'){
             window.alert("준비 중 입니다.")
         }
-        console.log("close nav")
     };
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
         console.log("open user")
     };
-    const handleCloseUserMenu = () => {
+    const handleCloseUserMenu = (setting : any) => {
         setAnchorElUser(null);
-        console.log("close user")
-    };
-    const handleUserMenuClick = (setting : any) => {
-        if(setting === '로그아웃'){
+        if(setting === '마이페이지'){
             // setIsLogin(!isLogin)
         }
-        if(setting === 'Size'){
-            console.log(w)
+        else if(setting === '작성기록'){
+
+        }
+        else if(setting === '설정'){
+
+        }
+        else if(setting === '로그아웃'){
+
+        }
+        else if(setting === 'Size'){
             window.alert(w)
         }
-        console.log("1")
-        // console.log(isLogin)
     }
 
     return(
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar sx={{pl: res400 ? 0 : '20px', height: '60px'}} >
+                <Toolbar sx={{pr: 0,pl: res450 ? 0 : '20px', height: '60px', justifyContent: 'space-around', alignContent: 'center', alignItems: 'center'}} >
                     {/*Desktop Display*/}
                     <Typography
                         variant="h6"
@@ -101,7 +102,7 @@ const Header = () => {
                         / 음자리 /
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' }, width: '64px' }}>
                         <IconButton
                             size="large"
                             aria-label="account of current user"
@@ -152,6 +153,8 @@ const Header = () => {
                             letterSpacing: '.3rem',
                             color: 'inherit',
                             textDecoration: 'none',
+                            textAlign: 'center',
+                            justifyContent: 'center'
                         }}
                     >
                         / 음자리 /
@@ -171,34 +174,44 @@ const Header = () => {
                         ))}
                     </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={() => handleUserMenuClick(setting)}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+                    <Box sx={{ flexGrow: 0, display: 'flex' }}>
+                        {
+                            userState.isLogin ?
+                                <>
+                                    <Tooltip title="Open settings">
+                                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Menu
+                                        sx={{ mt: '45px' }}
+                                        id="menu-appbar"
+                                        anchorEl={anchorElUser}
+                                        anchorOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        open={Boolean(anchorElUser)}
+                                        onClose={handleCloseUserMenu}
+                                    >
+                                        {settings.map((setting) => (
+                                            <MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
+                                                <Typography textAlign="center">{setting}</Typography>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+                                </>
+                                :
+                                res450 ?
+                                    <Button variant="contained" size={"small"} sx={{left: -10, position: 'relative', border: '0.5px solid white', fontSize: 8}}>로그인</Button>
+                                    :
+                                    <Button variant="contained" sx={{border: '1px solid white'}}>로그인</Button>
+                        }
                     </Box>
                 </Toolbar>
             </Container>
