6d18f796228052145a7e231d9d61747b9104b6c3
ongdyub <ongdyub>
2023-04-30 15:43:56 +0900

    temp

--- src/Concert/ConcertDetail/ConcertDetail.tsx
+++ src/Concert/ConcertDetail/ConcertDetail.tsx
@@ -1,19 +1,41 @@
 import {Button, ButtonGroup, Stack} from "@mui/material";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useParams} from "react-router-dom";
 import Visit from "../../MyConcert/MyHome/Visit/Visit";
 import DetailInfo from "./DetailInfo/DetailInfo";
 import Review from "./Review/Review";
 import ConcertComment from "./ConcertComment/ConcertComment";
 import ConcertMember from "./ConcertMember/ConcertMember";
+import {useNavigate} from "react-router";
+import {useState} from "react";
 
-const myButtons = [
-    <Button key="info">공연정보</Button>,
-    <Button key="review">리뷰</Button>,
-    <Button key="comment">기대평</Button>,
-    <Button key="member">연주자</Button>,
-]
+
 
 const ConcertDetail = () => {
+
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const onClickGoInfo = () => {
+        navigate(`/concert/${id}/info`)
+    }
+    const onClickGoReview = () => {
+        navigate(`/concert/${id}/review`)
+    }
+    const onClickGoComment = () => {
+        navigate(`/concert/${id}/comment`)
+    }
+    const onClickGoMember = () => {
+        navigate(`/concert/${id}/member`)
+    }
+
+    const myButtons = [
+        <Button onClick={onClickGoInfo} key="info">공연정보</Button>,
+        <Button onClick={onClickGoReview} key="review">리뷰</Button>,
+        <Button onClick={onClickGoComment} key="comment">기대평</Button>,
+        <Button onClick={onClickGoMember} key="member">연주자</Button>,
+    ]
+
+
+
     return(
         <Stack sx={{mt:1, pl: 3}}>
             <Stack sx={{mb: 3}}>
