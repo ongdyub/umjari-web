5bb817ad52214e5cb067335d3a291f44144d4dde
ongdyub <ongdyub>
2023-05-13 17:40:57 +0900

    qna profile image deesign fix

--- src/Group/GroupBoard/GroupQnAItem/GroupQnAComment.tsx
+++ src/Group/GroupBoard/GroupQnAItem/GroupQnAComment.tsx
@@ -37,7 +37,8 @@ const GroupQnAComment = (props : any) => {
                             component="img"
                             sx={{
                                 width: '100%',
-                                height: '100%'
+                                height: '100%',
+                                objectFit: 'cover'
                             }}
                             alt="Profile Img"
                             onError={({currentTarget}) => currentTarget.src = `${process.env.PUBLIC_URL}/Logo_posit.png`}
