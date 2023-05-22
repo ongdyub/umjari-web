5578ba314fc4fd4ebd6c8712344a1a12293fef32
ongdyub <ongdyub>
2023-05-22 14:48:12 +0900

    concert member loading

--- src/Concert/ConcertDetail/ConcertMember/ConcertMember.tsx
+++ src/Concert/ConcertDetail/ConcertMember/ConcertMember.tsx
@@ -1,5 +1,10 @@
 import {Avatar, Button, Chip, Divider, Stack, Typography, useMediaQuery, useTheme} from "@mui/material";
 import { FaCrown } from 'react-icons/fa';
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "../../../store";
+import {concertMemberGet, selectConcert} from "../../../store/slices/concert/concert";
 
 const playerData = [
     {
@@ -78,40 +83,40 @@ const ConcertMember = () => {
 
     const theme = useTheme();
     const res500 = useMediaQuery(theme.breakpoints.down("res500"))
-    const res800 = useMediaQuery(theme.breakpoints.down("res800"))
-    const res750 = useMediaQuery(theme.breakpoints.down("res750"))
+    const { id } = useParams();
+    const dispatch = useDispatch<AppDispatch>();
+
+    const memberState = useSelector(selectConcert)
+
+    useEffect(() => {
+        dispatch(concertMemberGet(id))
+    },[id,dispatch])
 
     return (
         <Stack justifyContent={"flex-start"}>
             <Divider sx={{width: '90%', mt: -1}}/>
-            <Stack sx={{mt: 5, pl:5, mb:10}}>
+            <Stack sx={{mt: 2, pl:5, pr: 5, mb:10}}>
                 {
-                    playerData.map((item) => (
-                        <Stack sx={{mb:5}}>
-                            <Stack direction={"row"} alignContent={"center"} justifyContent={"flex-start"} alignItems={"center"} sx={{mb:2}} flexWrap={"wrap"}>
-                                <Stack justifyContent={"flex-start"}>
-                                    <Typography sx={{fontSize: 45, fontWeight: 100, fontFamily: "Open Sans", mr:5}}>{item.role}</Typography>
-                                </Stack>
-                                <Stack direction={"row"} alignContent={"center"} justifyContent={"flex-start"} flexWrap={"wrap"}>
-                                    {
-                                        item.head.map((item, idx) => (
-                                            <Stack direction="row" spacing={5} sx={{mr:2, mb: res500 ? 1 : 0,mt: res500 ? 1 : 0,cursor: 'pointer', height: 48, width: 160}}>
-                                                <Stack sx={{borderRadius: 20, bgcolor: '#212121',pt:1, pl:1.5, pr:3, pb:1}} direction={"row"} alignItems={"center"} alignContent={"center"}>
-                                                    <Avatar alt={item} src={`/img/${idx*25}.jpg`} sx={{width: 33, height: 33}} />
-                                                    <Typography sx={{ml: 2, fontSize: 15, fontWeight: 400, color:'#ffffff', mr:1}}>{item}</Typography>
-                                                    <FaCrown color={'#ffffff'}/>
-                                                </Stack>
-                                            </Stack>
-                                        ))
-                                    }
-                                </Stack>
+                    memberState.participants.map((item, idx) => (
+                        <Stack key={idx} sx={{mb:2}}>
+                            <Stack justifyContent={"flex-start"}>
+                                <Typography sx={{fontSize: 35, fontWeight: 100, fontFamily: "Open Sans", mr:5}}>{item.part}</Typography>
                             </Stack>
-                            <Stack sx={{mb:0.5}} direction={"row"} justifyContent={"flex-start"} flexWrap={"wrap"}>
+                            <Stack direction={"row"} alignContent={"center"} justifyContent={"flex-start"} flexWrap={'wrap'} sx={{width : '100%'}}>
+                                {item.master.map((item) => (
+                                        <Stack key={item.id} direction="row" spacing={5} sx={{mr:2, mb: res500 ? 1 : 0,mt: res500 ? 1 : 0,cursor: 'pointer', height: 48, width: 160}}>
+                                            <Stack sx={{borderRadius: 20, bgcolor: '#212121',pt:1, pl:1.5, pr:3, pb:1}} direction={"row"} alignItems={"center"} alignContent={"center"}>
+                                                <Avatar alt={item.profileName} src={`${item.profileImage}`} sx={{width: 33, height: 33}} />
+                                                <Typography sx={{ml: 2, fontSize: 15, fontWeight: 400, color:'#ffffff', mr:1}}>{item.profileName}</Typography>
+                                                <FaCrown color={'#ffffff'}/>
+                                            </Stack>
+                                        </Stack>
+                                ))}
                                 {item.member.map((item,idx) => (
-                                    <Stack direction="row" spacing={5} sx={{mr:2, mb:2, cursor: 'pointer', height: 48, width: 130}}>
+                                    <Stack key={idx} direction="row" spacing={5} sx={{mr:2, mb:2, cursor: 'pointer', height: 48, width: 130}}>
                                         <Stack sx={{borderRadius: 20, bgcolor: '#eeeeee',pt:1, pl:1, pr:2.5, pb:1}} direction={"row"} alignItems={"center"} alignContent={"center"}>
-                                            <Avatar alt={item} src={`/img/${idx*25}.jpg`} sx={{width: 33, height: 33}} />
-                                            <Typography sx={{ml: 2, fontSize: 15, fontWeight: 400, color:'#424242'}}>{item}</Typography>
+                                            <Avatar alt={item.profileName} src={`${item.profileImage}`} sx={{width: 33, height: 33}} />
+                                            <Typography sx={{ml: 2, fontSize: 15, fontWeight: 400, color:'#424242'}}>{item.profileName}</Typography>
                                         </Stack>
                                     </Stack>
                                 ))}
