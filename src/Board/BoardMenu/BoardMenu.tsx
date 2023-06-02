1a556b150063e8ca7209e7e1f476308fbe5c0b10
ongdyub <ongdyub>
2023-06-03 00:33:10 +0900

    fix menu color

--- src/Board/BoardMenu/BoardMenu.tsx
+++ src/Board/BoardMenu/BoardMenu.tsx
@@ -75,23 +75,23 @@ const BoardMenu = () => {
     const { boardName } = useParams();
     const res800 = useMediaQuery('(max-width:800px)')
 
-    const [selectedIndex, setSelectedIndex] = useState(boardName)
+    const [selectedIndex, setSelectedIndex] = useState<string | undefined>(boardName)
     const [open, setOpen] = useState(false)
     const menuOpen = () => {
         setOpen((!open))
     }
     const handleMiniMenu = (item : any) => {
-        setSelectedIndex(item.ID)
+        setSelectedIndex(item.name)
         setOpen(!open)
         navigate(`/community/${item.name.replace(/(\s*)/g,'')}`)
     }
 
     return(
-        <Stack justifyContent="flex-start" alignItems="center" sx={{height: res800 ? 'auto' : '1000px', width: res800 ? '100%' : '160px', bgcolor: '#292929', minWidth: '160px' }}>
+        <Stack justifyContent="flex-start" alignItems="center" sx={{height: res800 ? '65px' : '1000px', width: res800 ? '100%' : '160px', bgcolor: '#292929', minWidth: '160px' }}>
             {res800 ?
                 <List component="nav" sx={{width: '100%', justifyContent:"flex-start", alignItems:"center"}}>
-                    {boardList.filter((item) => (item.name === selectedIndex)).map((item) => (
-                        <Stack onClick={menuOpen} justifyContent="flex-start" alignItems="center" sx={{width: '100%'}}>
+                    {boardList.filter((item) => (item.name === selectedIndex)).map((item, idx) => (
+                        <Stack key={idx} onClick={menuOpen} justifyContent="flex-start" alignItems="center" sx={{width: '100%'}}>
                             <Divider sx={{width: '40%', border: 'solid 0.4px white'}} />
                             <ListItemButton sx={{width: '100%', display: 'flex', justifyContent:"center", alignItems:"center"}}>
                                 <Typography sx={{pt:1, pb: 1, color: 'white', width: 'auto'}}>
@@ -102,8 +102,8 @@ const BoardMenu = () => {
                         </Stack>
                     ))}
                     <Collapse in={open} timeout="auto" unmountOnExit>
-                        {boardList.map((item) => (
-                            <Stack onClick={() => handleMiniMenu(item)} justifyContent="flex-start" alignItems="center" sx={{width: '100%', bgcolor: selectedIndex === item.name ? 'primary.light' : ''}}>
+                        {boardList.map((item, idx) => (
+                            <Stack key={idx} onClick={() => handleMiniMenu(item)} justifyContent="flex-start" alignItems="center" sx={{width: '100%', bgcolor: selectedIndex === item.name ? 'primary.light' : 'primary.main'}}>
                                 <Divider sx={{width: '40%', border: 'solid 0.4px white'}} />
                                 <ListItemButton sx={{width: '100%', display: 'flex', justifyContent:"center", alignItems:"center"}}>
                                     <Typography sx={{pt:1, pb: 1, color: 'white', width: 'auto'}}>
@@ -116,8 +116,8 @@ const BoardMenu = () => {
                 </List>
                 :
                 <List component="nav" sx={{width: '100%', justifyContent:"flex-start", alignItems:"center"}}>
-                    {boardList.map((item) => (
-                        <Stack onClick={() => handleMiniMenu(item)} justifyContent="flex-start" alignItems="center" sx={{width: '100%', bgcolor: selectedIndex === item.name ? 'primary.light' : ''}}>
+                    {boardList.map((item, idx) => (
+                        <Stack key={idx} onClick={() => handleMiniMenu(item)} justifyContent="flex-start" alignItems="center" sx={{width: '100%', bgcolor: selectedIndex === item.name ? 'primary.light' : ''}}>
                             <Divider sx={{width: '40%', border: 'solid 0.4px white'}} />
                             <ListItemButton sx={{width: '100%', display: 'flex', justifyContent:"center", alignItems:"center"}}>
                                 <Typography sx={{pt:1, pb: 1, color: 'white', width: 'auto'}}>
